b1d53d0ad3ebf22a8f05c837e9a4558dbdd3b3fa
Allisson Nunes <allissonabn@gmail.com>
2020-09-02 20:14:01 -0300

    Implementação do desafio fundamentos reactjs

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,27 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.goBack();
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const selectedFiles = files.map<FileProps>(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size, { base: 10, locale: 'pt-BR' }),
+    }));
+
+    setUploadedFiles(selectedFiles);
   }
 
   return (
